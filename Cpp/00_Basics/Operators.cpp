21418d23c92fb31c2fc197dea48379bddcfe8a79
amarpratapsingh <amarsinghpratap00058@gmail.com>
2025-05-16 13:00:05 +0530

    Modified

--- Cpp/00_Basics/Operators.cpp
+++ Cpp/00_Basics/Operators.cpp
@@ -2,17 +2,29 @@
 int main()
 {
     using std::cout;
+    using std::cin;
     using std::endl;
     int x = 10;
     int y = 20;
+    int val;
     int s = x+y;
     int d = x-y;
     int p = x*y;
     int q = x/y;
     int r = y%x;
-    cout<<s<<endl;
-    cout<<d<<endl;
-    cout<<p<<endl;
-    cout<<q<<endl;
-    cout<<r<<endl;
+    cout<<"Enter a value: ";
+    cin>>val;
+    if(val>= 1 && val<=10)
+    {
+        cout<<"In between 1 and 10"<<endl;
+    }
+    else
+    {
+        cout<<"Not in between"<<endl;
+    }
+    cout<<"Sum is: "<<s<<endl;
+    cout<<"Difference is: "<<d<<endl;
+    cout<<"Product is: "<<p<<endl;
+    cout<<"Quotient is: "<<q<<endl;
+    cout<<"Remainder is: "<<r<<endl;
 }
--- Cpp/00_Basics/Switch.cpp
+++ Cpp/00_Basics/Switch.cpp
@@ -2,18 +2,33 @@
 int main()
 {
     int month;
-    std::cout<<"Enter month: ";
+    std::cout<<"Enter month number: ";
     std::cin>>month;
     switch(month)
     {
-    case 1:
-    case 2:
-    
-        /* code */
-        break;
-    
-    default:
-        break;
+        case 1:
+        case 2:
+        case 12:
+            std::cout<<"Winter"; 
+            break;
+        case 3:
+        case 4:
+        case 5:
+            std::cout<<"Spring";
+            break;
+        case 6:
+        case 7:
+        case 8:
+            std::cout<<"Summer";
+            break;
+        case 9:
+        case 10:
+        case 11:
+            std::cout<<"Autumn";
+            break;
+        default:
+            std::cout<<"Invalid Input";
+            break;
     }
     return 0;
 }
